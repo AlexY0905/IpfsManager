8ac2e302ad2dfbf3b4e07d26722fad58b2145568
sizing-wang <1395041594@qq.com>
2020-11-13 17:56:36 +0800

    优化

--- src/util/index.js
+++ src/util/index.js
@@ -1,10 +1,10 @@
 //在浏览器缓存用户信息
 export const setUsername = (username) => {
-    window.localStorage.setItem("username", username)
+    window.localStorage.setItem("lotusUsername", username)
 };
 export const getUsername = () => {
-    return window.localStorage.getItem("username")
+    return window.localStorage.getItem("lotusUsername")
 };
 export const removeUsername = () => {
-    return window.localStorage.removeItem("username")
+    return window.localStorage.removeItem("lotusUsername")
 }; 
